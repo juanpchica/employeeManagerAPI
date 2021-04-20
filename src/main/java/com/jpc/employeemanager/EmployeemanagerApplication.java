9cc59952048c93095419e04e4d3d6740d169f2d2
Juan Pablo Chica <juanpchica@hotmail.com>
2021-04-20 14:05:34 -0500

    New dependencies for oracle jdbc added and port web server changed

--- src/main/java/com/jpc/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/jpc/employeemanager/EmployeemanagerApplication.java
@@ -2,12 +2,19 @@ package com.jpc.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
 
+	@GetMapping
+	public String Hello(){
+		return "Hello World";
+	}
 }
